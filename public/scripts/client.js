b483e6506d284007d56ee2a865231b73bd28d74f
Jingu Kang <jinguk0816@gmail.com>
2021-04-21 19:07:56 -0400

    performs an action when pressing a tweet button on a page then uploads the tweet to the page. also protects from Cross-Site Scriting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,40 +3,23 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = function(tweets) {
+  //deletes the existing tweet so that only the new tweet will be posted.
+  $('#tweets-container').empty();
   //loops through tweets
-  for(const tweet of tweets){
+  for (const tweet of tweets) {
     //calls createTweetElement for each tweets
     const newTweet = createTweetElement(tweet);
     //takes return value and appends it to the tweets container
-    $('#tweets-container').append(newTweet);
+    $('#tweets-container').prepend(newTweet);
   }
-}
+};
+
+const escape = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const createTweetElement = function(tweet) {
   let $tweet = `
@@ -48,9 +31,9 @@ const createTweetElement = function(tweet) {
       </div>
       <p>${tweet.user.handle}</p>
     </header>
-    <p>${tweet.content.text}</p>
+    <p>${escape(tweet.content.text)}</p>
     <footer>
-      <span class="need_to_be_rendered" datetime="${tweet.created_at}">${moment(tweet.created_at).fromNow()}</span>
+      <span class="need_to_be_rendered" datetime="${tweet.created_at}"></span>
       <div class=icons-in-tweet>
         <i class="fas fa-flag"></i>
         <div>
@@ -60,17 +43,40 @@ const createTweetElement = function(tweet) {
       </div>
     </footer>
   </article>
-`
+`;
   return $tweet;
-}
+};
+
+const loadTweets = function() {
+  const submitButton = $('button');
+  $.ajax('/tweets', { method: 'GET' })
+    .then(function(moreTweet) {
+      renderTweets(moreTweet);
+      timeago.render(document.querySelectorAll('.need_to_be_rendered'));
+    })
+    .catch((e) => console.log(("error: ", e)));
+};
+
+const submitTweets = function() {
+
+  $("#tweet-input").on('submit', function(event) {
+    event.preventDefault();
+    if ($('#tweet-text').val() === "") {
+      alert("Text is empty.");
+    } else if ($('#tweet-text').val().length > 141) {
+      alert("Your text exceeds 140 characters.");
+    } else {
+      $.ajax('/tweets', { method: 'POST', data: $(this).serialize() })
+        .then(() => {
+          location.reload();
+          loadTweets();
+        })
+        .catch((error) => console.log("error: " , error));
+    }
+  });
+};
 
 $(document).ready(function() {
-  renderTweets(data);
-  $("#tweet-input").on('submit', function(event){
-    event.preventDefault();
-    $.ajax('/tweets/', { method: 'POST', data: $(this).serialize() })
-    .then(() => {console.log($(this).serialize())})
-    .catch((error) => console.log("error: " , error))
-  });
-  timeago.render(document.querySelectorAll('.need_to_be_rendered'));
+  submitTweets();
+  loadTweets();
 });
