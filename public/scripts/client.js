62ebc9c8dfb20377364ec4aebc08f22a38096182
Jingu Kang <jinguk0816@gmail.com>
2021-04-22 18:23:57 -0400

    add comments so it's easier to see what is happening inside functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,7 +14,7 @@ const renderTweets = function (tweets) {
     $("#tweets-container").prepend(newTweet);
   }
 };
-
+//Escape function is used to protect from cross-site scripting.
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -48,10 +48,10 @@ const createTweetElement = function (tweet) {
 };
 
 const loadTweets = function () {
-  const submitButton = $("button");
   $.ajax("/tweets", { method: "GET" })
     .then(function (moreTweet) {
       renderTweets(moreTweet);
+      //This uses the timeago.js to update the time of tweets created.
       timeago.render(document.querySelectorAll(".need_to_be_rendered"));
     })
     .catch((e) => console.log(("error: ", e)));
@@ -61,18 +61,20 @@ const submitTweets = function () {
   $("#tweet-input").on("submit", function (event) {
     event.preventDefault();
     const textInput = $("#tweet-text").val();
+    //validation for input field
     if (textInput.trim().length === 0) {
-      $( ".hide-exceed" ).slideUp( "slow", function() {      
-        $( ".hide" ).slideDown( "slow", function() {});
+      $(".hide-exceed").slideUp("slow", function () {
+        $(".hide").slideDown("slow", function () {});
       });
     } else if ($("#tweet-text").val().length > 141) {
-      $( ".hide" ).slideUp( "slow", function() {
-        $( ".hide-exceed" ).slideDown( "slow", function() {});
+      $(".hide").slideUp("slow", function () {
+        $(".hide-exceed").slideDown("slow", function () {});
       });
     } else {
       $.ajax("/tweets", { method: "POST", data: $(this).serialize() })
         .then(() => {
           $(".new-tweet span").slideUp();
+          //reset the input text and as well as the word counter.
           $("#tweet-text").val("");
           $(".counter").val(140);
           loadTweets();
@@ -81,25 +83,25 @@ const submitTweets = function () {
     }
   });
 };
-
-const slideNewTweet = function() {
+//Function that toggles the compose box.
+const slideNewTweet = function () {
   let isTweetTextOpen = false;
-  $(".nav-new-tweet").on("click", function(event){
-    isTweetTextOpen = !isTweetTextOpen;
-    if(isTweetTextOpen) {
-      $(".new-tweet").slideDown("slow", function(){
+  $(".nav-new-tweet").on("click", function (event) {
+    isTweetTextOpen = !isTweetTextOpen; //This line changes the value of the variable to false and true every time the button is clicked
+    if (isTweetTextOpen) {
+      $(".new-tweet").slideDown("slow", function () {
         $(".nav-new-tweet").css("opacity", "0.5");
         $("#tweet-text").focus();
       });
     }
-    if(!isTweetTextOpen){
-      $( ".new-tweet" ).slideUp( "slow", function() {
+    if (!isTweetTextOpen) {
+      $(".new-tweet").slideUp("slow", function () {
         $(".nav-new-tweet").css("opacity", "1");
       });
     }
   });
-}
-
+};
+//Calls functions when the HTML document is ready.
 $(document).ready(function () {
   slideNewTweet();
   submitTweets();
